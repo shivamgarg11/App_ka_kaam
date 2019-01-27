1b89abe099781256e4b389e9eeb40eae1ee3ba6e
Vrishank <vrishank98@gmail.com>
2019-01-28 00:24:48 +0530

    Permission
    Signed-off-by: Vrishank <vrishank98@gmail.com>

--- app/src/main/java/com/shivam/app_ka_kaam/MainActivity.java
+++ app/src/main/java/com/shivam/app_ka_kaam/MainActivity.java
@@ -3,13 +3,21 @@ package com.shivam.app_ka_kaam;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.shivam.app_ka_kaam.ADMIN.admin;
 
@@ -27,13 +35,91 @@ import java.util.ArrayList;
 import java.util.List;
 import com.shivam.app_ka_kaam.R;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_REQUEST_CODE = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        if(!checkPermission()){
+            requestPermission();
+        }
+        else
+        {
+            afterPerm();
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+            switch (requestCode) {
+                case PERMISSION_REQUEST_CODE:
+                    if (grantResults.length > 0) {
+
+                        boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                        if (locationAccepted)  afterPerm();
+
+                        else {
+
+                            Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
+                                    showMessageOKCancel("You need to allow access to the permissions",
+                                            new DialogInterface.OnClickListener()
+                                            {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                        requestPermissions(new String[]{ACCESS_FINE_LOCATION},
+                                                                PERMISSION_REQUEST_CODE);
+                                                    }
+                                                }
+                                            });
+                                    return;
+                                }
+                            }
+
+                        }
+                    }
+
+
+                    break;
+            }
+    }
+
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private void afterPerm()
+    {
         ImageView adminbtn=findViewById(R.id.admin);
         ImageView userbtn=findViewById(R.id.user);
 
@@ -52,20 +138,10 @@ public class MainActivity extends AppCompatActivity {
         userbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-       startActivity(new Intent(MainActivity.this,user.class));
-       finish();
+                startActivity(new Intent(MainActivity.this,user.class));
+                finish();
             }
         });
-
-
-
-
-
-
-
-
-
-
         //////////////////////////////////////////////////////////
 
 //        String s = String.valueOf(FirebaseInstanceId.getInstance().getToken());
@@ -82,76 +158,75 @@ public class MainActivity extends AppCompatActivity {
             channel.setVibrationPattern(new long[] {100,200,300,400,500,400,300,200,400});
             notificationManager.createNotificationChannel(channel);
         }
-
-
-
     }
 
+    public void csvPart()
+    {
+        Employee emp1 = new Employee(1, "FirstName1", "LastName1", 10000);
+        Employee emp2 = new Employee(2, "FirstName2", "LastName2", 20000);
+        Employee emp3 = new Employee(3, "FirstName3", "LastName3", 30000);
+        Employee emp4 = new Employee(4, "FirstName4", "LastName4", 40000);
+        Employee emp5 = new Employee(5, "FirstName5", "LastName5", 50000);
 
-public void csvPart() {
-    Employee emp1 = new Employee(1, "FirstName1", "LastName1", 10000);
-    Employee emp2 = new Employee(2, "FirstName2", "LastName2", 20000);
-    Employee emp3 = new Employee(3, "FirstName3", "LastName3", 30000);
-    Employee emp4 = new Employee(4, "FirstName4", "LastName4", 40000);
-    Employee emp5 = new Employee(5, "FirstName5", "LastName5", 50000);
-
-    //Add Employee objects to a list
-    List empList = new ArrayList();
-    empList.add(emp1);
-    empList.add(emp2);
-    empList.add(emp3);
-    empList.add(emp4);
-    empList.add(emp5);
+        //Add Employee objects to a list
+        List empList = new ArrayList();
+        empList.add(emp1);
+        empList.add(emp2);
+        empList.add(emp3);
+        empList.add(emp4);
+        empList.add(emp5);
 
 
-    String a = empList.get(0).toString();
-    a = a + "\n" + empList.get(1).toString();
+        String a = empList.get(0).toString();
+        a = a + "\n" + empList.get(1).toString();
 
-//        String a = "1,2,4,5,6";
-    Log.d("MessageFirst A", "onCreate: " + a);
-    String filePath = getApplicationContext().getFilesDir().getPath().toString() + "/Employee.csv";
-    try {
-        String content = "Separe here integers by semi-colon";
-        File file = new File(filePath);
-        // if file doesnt exists, then create it
+    //        String a = "1,2,4,5,6";
+        Log.d("MessageFirst A", "onCreate: " + a);
+        String filePath = getApplicationContext().getFilesDir().getPath().toString() + "/Employee.csv";
         try {
-            if (!file.exists()) {
-                new File(file.getParent()).mkdirs();
-                file.createNewFile();
+            String content = "Separe here integers by semi-colon";
+            File file = new File(filePath);
+            // if file doesnt exists, then create it
+            try {
+                if (!file.exists()) {
+                    new File(file.getParent()).mkdirs();
+                    file.createNewFile();
+                }
+            } catch (IOException e) {
+                Log.e("", "Could not create file.", e);
+                return;
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(a);
+            Log.d("MessageHere", a);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Check for Success by reading
+
+        String b = "";
+        BufferedReader br = null;
+        try {
+            String sCurrentLine;
+            br = new BufferedReader(new FileReader(getApplicationContext().getFilesDir().getPath().toString() + "/Employee.csv"));
+            while ((sCurrentLine = br.readLine()) != null) {
+                b = b + sCurrentLine;
             }
         } catch (IOException e) {
-            Log.e("", "Could not create file.", e);
-            return;
+            e.printStackTrace();
         }
-        FileWriter fw = new FileWriter(file.getAbsoluteFile());
-        BufferedWriter bw = new BufferedWriter(fw);
-        bw.write(a);
-        Log.d("MessageHere", a);
-        bw.close();
-
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-
-    //Check for Success by reading
-
-    String b = "";
-    BufferedReader br = null;
-    try {
-        String sCurrentLine;
-        br = new BufferedReader(new FileReader(getApplicationContext().getFilesDir().getPath().toString() + "/Employee.csv"));
-        while ((sCurrentLine = br.readLine()) != null) {
-            b = b + sCurrentLine;
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    } finally {
-        try {
-            if (br != null) br.close();
-            Log.d("MessageHereRead", "onCreate: " + b);
-        } catch (IOException ex) {
-            Log.d("MainActivity.java", "csvPart: Error");
-            }
+        finally
+        {
+            try {
+                if (br != null) br.close();
+                Log.d("MessageHereRead", "onCreate: " + b);
+            } catch (IOException ex) {
+                Log.d("MainActivity.java", "csvPart: Error");
+                }
         }
     }
 }
--- app/src/main/java/com/shivam/app_ka_kaam/sampleUtil/MyNotificationManager.java
+++ app/src/main/java/com/shivam/app_ka_kaam/sampleUtil/MyNotificationManager.java
@@ -1,6 +1,5 @@
 package com.shivam.app_ka_kaam.sampleUtil;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
