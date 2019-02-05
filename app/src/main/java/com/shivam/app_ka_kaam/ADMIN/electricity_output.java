cb121d925a6ced7fc59a5030f0daf3e67f648ca8
Shivam Garg <contestshivamgarg@gmail.com>
2019-02-05 21:07:19 +0530

    Admin UI changed with per day summary

--- app/src/main/java/com/shivam/app_ka_kaam/ADMIN/electricity_output.java
+++ app/src/main/java/com/shivam/app_ka_kaam/ADMIN/electricity_output.java
@@ -13,16 +13,11 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.shashank.sony.fancytoastlib.FancyToast;
-import com.shivam.app_ka_kaam.Java_objects.electricityconstants;
+import com.shivam.app_ka_kaam.Fragments.electricitysummaryfrag;
 import com.shivam.app_ka_kaam.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class electricity_output extends AppCompatActivity {
@@ -39,18 +34,45 @@ public class electricity_output extends AppCompatActivity {
         path.setText("ADMIN/ELECTRICITY/"+pathway);
 
 
-        Button goback=findViewById(R.id.goback);
-        goback.setOnClickListener(new View.OnClickListener() {
+        /////////////////////////////
+
+
+        Button today=findViewById(R.id.today);
+        today.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(electricity_output.this, admin.class));
-                finish();
+                Calendar c = Calendar.getInstance();
+                SimpleDateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
+                final String todaydate = dateformat.format(c.getTime());
+                String passingstr =todaydate.substring(0,4)+todaydate.substring(5,7)+todaydate.substring(8);
+                android.app.FragmentManager fragmentManager = getFragmentManager();
+                electricitysummaryfrag frag = new electricitysummaryfrag(passingstr,electricity_output.this,pathway);
+                fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
+            }
+        });
+
+        today.performClick();
+
+        Button yesterday=findViewById(R.id.yesterday);
+        yesterday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                c.add(Calendar.DATE,-1);
+                SimpleDateFormat dateformat = new SimpleDateFormat("yyyy/MM/dd");
+                final String yesterdaydate = dateformat.format(c.getTime());
+                String passingstr =yesterdaydate.substring(0,4)+yesterdaydate.substring(5,7)+yesterdaydate.substring(8);
+                android.app.FragmentManager fragmentManager = getFragmentManager();
+                electricitysummaryfrag frag = new electricitysummaryfrag(passingstr,electricity_output.this,pathway);
+                fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
             }
         });
 
 
 
         final String[] date = {""};
+
+
         Button summary=findViewById(R.id.summary);
         summary.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,17 +84,16 @@ public class electricity_output extends AppCompatActivity {
                             date[0] =""+year+"0"+(monthOfYear+1)+dayOfMonth;
                         else
                             date[0] =""+year+(monthOfYear+1)+dayOfMonth;
-                        Intent i=new Intent(electricity_output.this,electricitysummary.class);
-                        i.putExtra("DATE",date[0]);
-                        i.putExtra("pathway",pathway);
-                        startActivity(i);
-                        finish();
+                        android.app.FragmentManager fragmentManager = getFragmentManager();
+                        electricitysummaryfrag frag = new electricitysummaryfrag(date[0],electricity_output.this,pathway);
+                        fragmentManager.beginTransaction().replace(R.id.frame, frag).commit();
                     }
                 }, 2019, 01, 01).show();
             }
         });
 
 
+        //////////////////////////////////
         Button setting =findViewById(R.id.setting);
         setting.setOnClickListener(new View.OnClickListener() {
             String []arr={"CHANGE CONSTANTS","CHANGE RANGE","PASSWORD RESET"};
@@ -92,7 +113,7 @@ public class electricity_output extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 if(selected==0){
-                                    changeconstant();
+                                   // changeconstant();
                                 }
 
 
@@ -114,90 +135,19 @@ public class electricity_output extends AppCompatActivity {
 
 
 
-    }
-
-
-
-    public void changeconstant(){
-        // get prompts.xml view
-        LayoutInflater li = LayoutInflater.from(electricity_output.this);
-        View changeconstant = li.inflate(R.layout.setelectricityconstants, null);
-
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                electricity_output.this);
-
-        // set prompts.xml to alertdialog builder
-        alertDialogBuilder.setView(changeconstant);
-
-        final EditText c1 = (EditText) changeconstant
-                .findViewById(R.id.c1);
-        final EditText c2 = (EditText) changeconstant
-                .findViewById(R.id.c2);
-        final EditText c3 = (EditText) changeconstant
-                .findViewById(R.id.c3);
-
-
-        // set dialog message
-        alertDialogBuilder
-                .setCancelable(false)
-                .setPositiveButton("OK",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-
-                                final String strc1=c1.getText().toString()+"";
-                                final String strc2=c2.getText().toString()+"";
-                                final String strc3=c3.getText().toString()+"";
-
-                                if(strc1.length()==0||strc2.length()==0||strc3.length()==0){
-                                    FancyToast.makeText(electricity_output.this,"INVALID INPUTS", Toast.LENGTH_SHORT,FancyToast.ERROR,false).show();
-                                }else{
-                                    final FirebaseDatabase database1 = FirebaseDatabase.getInstance();
-                                    final DatabaseReference myRef1 = database1.getReference("ELECTRICITY"+pathway).child("CONSTANTS");
-                                    electricityconstants con=new electricityconstants(Double.valueOf(strc1),Double.valueOf(strc2),Double.valueOf(strc3));
-                                    myRef1.setValue(con);
-                                }
-
-
-                            }
-                        })
-                .setNegativeButton("Cancel",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-        // create alert dialog
-        final AlertDialog alertDialog = alertDialogBuilder.create();
-
-        // show it
-
-
-
-
-        final FirebaseDatabase database1 = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef1 = database1.getReference("ELECTRICITY"+pathway).child("CONSTANTS");
-
-
-
-        myRef1.addListenerForSingleValueEvent(new ValueEventListener() {
+        Button goback=findViewById(R.id.goback);
+        goback.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-
-                electricityconstants con =dataSnapshot.getValue(electricityconstants.class);
-                c1.setText(con.getC1()+"");
-                c2.setText(con.getC2()+"");
-                c3.setText(con.getC3()+"");
-                alertDialog.show();
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("TAG", "Failed to read value.", error.toException());
+            public void onClick(View v) {
+                startActivity(new Intent(electricity_output.this, admin.class));
+                finish();
             }
         });
 
 
+
+
+        //////////////////////////////////
 
 
     }
